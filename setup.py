740e3e8b46f4fdc046f7bc7ed3109049f6d089a2
Benoît Coste <benoit.coste@epfl.ch>
2018-12-10 17:49:23 +0100

    Bump version number
    Change-Id: Ie4ebb5d3108262e5518749acad474008e3bbc58c

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ from setuptools import setup
 from setuptools import find_packages
 import pip
 
-VERSION = '0.0.1'
+VERSION = '1.0.0'
 
 config = {
     'version': VERSION,
     'description': 'TNS: synthesis of artificial neurons using their topological profiles package',
     'author': 'Lida Kanari',
-    'url': 'https://bitbucket.org/bbp_lida/morphsynthesis',
     'author_email': 'lida.kanari@epfl.ch',
     'install_requires': [
         'matplotlib>=1.3.1',
@@ -21,10 +20,10 @@ config = {
         'enum34>=1.0.4',
         'scipy>=0.13.3',
         'numpy>=1.8.0'
+        'morphio<=2.0.0'
     ],
     'dependency_links': [
         'git+ssh://bbpcode.epfl.ch/molecularsystems/TMD#egg=tmd-1.0.0',
-        'git+ssh://git@github.com/bluebrain/morphio#egg=morphio-0.9.4',
     ],
     'extras_require': {},
     'packages': find_packages(),
@@ -33,6 +32,5 @@ config = {
     'name': 'tns',
     'include_package_data': True,
 }
-
 
 setup(**config)
