0266d2163b098550d0817692f04ee6115ffee594
Benoît Coste <benoit.coste@epfl.ch>
2019-01-14 11:59:19 +0100

    Fix TMD version and update README
    Change-Id: I5f533277595e403846e39c01b86432b9a455a9db

--- README.md
+++ README.md
@@ -14,5 +14,5 @@ virtualenv test_syn
 source ./test_syn/bin/activate
 git clone ssh://bbpcode.epfl.ch/molecularsystems/TNS
 pip install --upgrade pip
-pip install ./TNS --process-dependency-links --index-url  https://bbpteam.epfl.ch/repository/devpi/bbprelman/dev/+simple/
+pip install ./TNS --index-url  https://bbpteam.epfl.ch/repository/devpi/bbprelman/dev/+simple/
 ```
--- setup.py
+++ setup.py
@@ -9,26 +9,24 @@ import pip
 
 VERSION = imp.load_source("tns.version", "tns/version.py").VERSION
 
-config = {
-    'version': VERSION,
-    'description': 'TNS: synthesis of artificial neurons using their topological profiles package',
-    'author': 'Lida Kanari',
-    'author_email': 'lida.kanari@epfl.ch',
-    'install_requires': [
+setup(
+    version= VERSION,
+    description='TNS= synthesis of artificial neurons using their topological profiles package',
+    author='Lida Kanari',
+    author_email='lida.kanari@epfl.ch',
+    install_requires=[
         'matplotlib>=1.3.1',
         'morphio>=2.0.0',
         'neurom>=1.4.7',
-        'tmd>=1.0.0',
+        'tmd>=2.0.3',
         'enum34>=1.0.4',
         'scipy>=0.13.3',
         'numpy>=1.8.0',
         'tmd==2.0.3',
     ],
-    'packages': find_packages(),
-    'license': 'BSD',
-    'scripts': [],
-    'name': 'tns',
-    'include_package_data': True,
-}
-
-setup(**config)
+    packages=find_packages(),
+    license='BSD',
+    scripts=[],
+    name='tns',
+    include_package_data=True,
+)
