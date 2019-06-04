Fix normalization bug and improve branching direction
Change-Id: Ib87f93bc0a8ac90f6d06c7963fe1aaf682fb0688

tests/test_algo.py
@@ -26,7 +26,7 @@ def _setup_test(Algo, Grower):
     np.random.seed(42)
     algo = Algo(distributions, parameters, [0, 0, 1])
 
-    grower = Grower(None, None, [1.1,0.,0.], [2, 2, 2], 0.2, 0.3, 'major',
+    grower = Grower(None, None, [1.1,0.,0.], [0.57735, 0.57735, 0.57735], 0.2, 0.3, 'major',
                            {'bif_term': {'bif': 9.7747, 'ref': [0, 0, 1], 'term': 159.798}})
     return algo, grower
 
@@ -72,13 +72,13 @@ def test_TMDApicalAlgo():
 
     s1, s2 = algo.bifurcate(grower)
     _assert_dict_or_array(s1,
-                          {'direction': [2., 2., 2.],
+                          {'direction': [0.57735, 0.57735, 0.57735],
                            'process': 'secondary',
                            'start_point': [1.1, 0. , 0. ],
                            'stop': {'bif_term': {'bif': 18.5246, 'ref': [0, 0, 1], 'term': 159.798}}})
 
     _assert_dict_or_array(s2,
-                          {'direction': np.array([0.704878, 1.874423, 2.826603]),
+                          {'direction': np.array([0.203481, 0.541099, 0.81597]),
                            'process': 'secondary',
                            'start_point': [1.1, 0. , 0. ],
                            'stop': {'bif_term': {'bif': 18.5246, 'ref': [0, 0, 1], 'term': 124.8796}}})
@@ -95,13 +95,13 @@ def test_TMDGradientAlgo():
 
     s1, s2 = algo.bifurcate(grower)
     _assert_dict_or_array(s1,
-                          {'direction': [2., 2., 2.],
+                          {'direction': [0.57735, 0.57735, 0.57735],
                            'process': 'major',
                            'start_point': [1.1, 0. , 0. ],
                            'stop': {'bif_term': {'bif': 18.5246, 'ref': [0, 0, 1], 'term': 159.798}}})
 
     _assert_dict_or_array(s2,
-                          {'direction': np.array([1.352439, 1.937212, 2.413301]),
+                          {'direction': np.array([0.390416, 0.559225, 0.69666 ]),
                            'process': 'major',
                            'start_point': [1.1, 0. , 0. ],
                            'stop': {'bif_term': {'bif': 18.5246, 'ref': [0, 0, 1], 'term': 124.8796}}})
@@ -138,13 +138,13 @@ def test_TMDApicalAlgoPath():
 
     s1, s2 = algo.bifurcate(grower)
     _assert_dict_or_array(s1,
-                          {'direction': [2., 2., 2.],
+                          {'direction': [0.57735, 0.57735, 0.57735],
                            'process': 'major',
                            'start_point': [1.1, 0. , 0. ],
                            'stop': {'bif_term': {'bif': 18.5246, 'ref': 0, 'term': 159.798}}})
 
     _assert_dict_or_array(s2,
-                          {'direction': np.array([0.704878, 1.874423, 2.826603]),
+                          {'direction': np.array([0.20348076, 0.54109933, 0.81597003]),
                            'process': 'secondary',
                            'start_point': [1.1, 0. , 0. ],
                            'stop': {'bif_term': {'bif': 18.5246, 'ref': 0, 'term': 124.8796}}})
@@ -161,13 +161,13 @@ def test_TMDGradientAlgoPath():
 
     s1, s2 = algo.bifurcate(grower)
     _assert_dict_or_array(s1,
-                          {'direction': [2., 2., 2.],
+                          {'direction': [0.57735, 0.57735, 0.57735],
                            'process': 'major',
                            'start_point': [1.1, 0. , 0. ],
                            'stop': {'bif_term': {'bif': 18.5246, 'ref': 0, 'term': 159.798}}})
 
     _assert_dict_or_array(s2,
-                          {'direction': np.array([1.352439, 1.937212, 2.413301]),
+                          {'direction': np.array([0.390416, 0.559225, 0.69666 ]),
                            'process': 'major',
                            'start_point': [1.1, 0. , 0. ],
                            'stop': {'bif_term': {'bif': 18.5246, 'ref': 0, 'term': 124.8796}}})

tests/test_generate_section.py
@@ -13,7 +13,7 @@ def test_MEMORY_WEIGHTS():
 
 def test_SectionGrower():
     # Create basic section for testing purposes
-    s = section.SectionGrower(None, None, [0.,0.,0.], None, 0.0, 0.0, None, None)
+    s = section.SectionGrower(None, None, [0.,0.,0.], [0., 1., 0.], 0.0, 0.0, None, None)
     # Test trivial history is zeros.
     assert_array_almost_equal(s.history(), np.zeros(3))
     # Test history of one point.

tns/generate/section.py
@@ -26,7 +26,8 @@ class SectionGrower(object):
         generates a tubular morphology that resembles a random walk.
         '''
         self.parent = parent
-        self.direction = direction
+        assert not np.isclose(vectorial_norm(direction), 0.0), 'Nan direction not recognized'
+        self.direction = direction / vectorial_norm(direction)
         self.children = children
         self.points = [np.array(start_point[:3])]
         self.params = {"randomness": randomness,
@@ -59,7 +60,7 @@ class SectionGrower(object):
     def history(self):
         '''Returns a combination of the sections history
         '''
-        n_points = min(MEMORY, len(self.points) - 1)
+        n_points = len(self.latest_directions_normed)
 
         if n_points == 0:
             return np.zeros(3)
@@ -67,7 +68,7 @@ class SectionGrower(object):
         hist = np.dot(WEIGHTS[MEMORY - n_points:], self.latest_directions_normed)
         distance = vectorial_norm(hist)
 
-        if distance > 0:
+        if not np.isclose(distance, 0.0):
             hist /= distance
 
         return hist