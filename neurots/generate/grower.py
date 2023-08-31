2968da5ac84f4fe4b93e9cdc612cb809929f26a5
arnaudon <alexis.arnaudon@epfl.ch>
2023-08-31 16:48:37 +0200

    bump morphio

--- neurots/generate/grower.py
+++ neurots/generate/grower.py
@@ -409,12 +409,7 @@ class NeuronGrower:
         self.neuron.soma.points = points
         self.neuron.soma.diameters = diameters
 
-        try:
-            # see https://github.com/BlueBrain/NeuroTS/issues/76
-            if soma_type == "contour":
-                self.neuron.soma.type = SomaType.SOMA_SIMPLE_CONTOUR
-            if soma_type == "one_point":
-                self.neuron.soma.type = SomaType.SOMA_SINGLE_POINT
-        except AttributeError:
-            # astrocytes don't allow this
-            pass
+        if soma_type == "contour":
+            self.neuron.soma.type = SomaType.SOMA_SIMPLE_CONTOUR
+        if soma_type == "one_point":
+            self.neuron.soma.type = SomaType.SOMA_SINGLE_POINT
