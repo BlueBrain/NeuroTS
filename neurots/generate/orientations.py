d547805654e142d543777eca1225fcec9ac9cc87
arnaudon <alexis.arnaudon@epfl.ch>
2023-09-28 15:04:33 +0200

    set for 2 apicals

--- neurots/generate/orientations.py
+++ neurots/generate/orientations.py
@@ -235,6 +235,9 @@ class OrientationManager(OrientationManagerBase):
         Pia direction can be overwritten by the parameter 'pia_direction' value.
         """
         n_orientations = sample.n_neurites(self._distributions[tree_type]["num_trees"], self._rng)
+        if len(values_dict["direction"]["mean"]) == n_orientations:
+            # to force the direction of possibly 2 apicals, otherwise it is for basals
+            n_orientations = 1
 
         angles = []
         for _ in range(n_orientations):
