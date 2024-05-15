78e284688f324972d14edd779ce87eec23aa5114
Adrien Berchet <adrien.berchet@epfl.ch>
2024-05-15 14:03:18 +0200

    Chore: Use SPDX identifier in file headers

--- neurots/preprocess/preprocessors.py
+++ neurots/preprocess/preprocessors.py
@@ -11,17 +11,7 @@ The preprocesses should be registered to be executed in the preprocess step usin
 
 # Copyright (C) 2022  Blue Brain Project, EPFL
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 
 from neurots.generate.orientations import fit_3d_angles
 from neurots.preprocess.utils import register_global_preprocessor
